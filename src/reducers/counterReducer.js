e4ab344ac656eed344f0364d5861d0a35ca21057
Saurabh <saurabhgawande2016@gmail.com>
2023-01-30 16:50:03 +0530

    Done

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,13 @@
-const counterReducer = (state,action) =>{
+const InitialState = {
+    counter : 0
+}
+const counterReducer = (state= InitialState,action) =>{
+switch(action.type){
+    case "INCREMENT": 
+    return{...state , counter: state.counter+1};
+    case "DECREMENT" : 
+    return{ ...state , counter : state.counter-1}
+}
 
 }
 
